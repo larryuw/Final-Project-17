8dc25578cb43d1e6e797c407d6ec16ad04f0ecda
Stefanie Meyer <sjmhoops@gmail.com>
2019-06-03 15:01:55 -0700

    updated

--- finalScript.R
+++ finalScript.R
@@ -1,5 +1,5 @@
-Pop_Dens_by_County <- read.csv("WAOFM_-_April_1_-_Population_Density_by_County__2000_to_Present.csv")
-HS_Dropout_by_County <- read.csv("High_School_Dropout_Statistics_by_County_2012-2013_School_Year_5-Year_Cohort_Dropout_Rates.csv")
+Pop_Dens_by_County <- read.csv("data/WAOFM_-_April_1_-_Population_Density_by_County__2000_to_Present.csv")
+HS_Dropout_by_County <- read.csv("data/High_School_Dropout_Statistics_by_County_2012-2013_School_Year_5-Year_Cohort_Dropout_Rates.csv")
 
 #home values
 #home listings and sales
@@ -8,8 +8,8 @@ HS_Dropout_by_County <- read.csv("High_School_Dropout_Statistics_by_County_2012-
 #forecasts
 #more metrics
 
-County_Zhvi_3b <- read.csv("County_Zhvi_3bedroom.csv")
-County_MedianRentalPrice_3B <- read.csv("County_MedianRentalPrice_3Bedroom.csv")
+County_Zhvi_3b <- read.csv("data/County_Zhvi_3bedroom.csv")
+County_MedianRentalPrice_3B <- read.csv("data/County_MedianRentalPrice_3Bedroom.csv")
 
 library(dplyr)
 County_Zhvi_3b <- filter(County_Zhvi_3b, State == "WA")
@@ -22,6 +22,7 @@ melted$rowid <- 1:30
 library(ggplot2)
 ggplot(melted, aes(variable, value, group=factor(RegionName))) +
   geom_line(aes(color=RegionName)) +
+  geom_smooth(method="lm",fullrange=TRUE,aes(col=RegionName)) +
   ggtitle("Washington Home Values by County") + 
   xlab("Year (1996-2019)") +
   ylab("Value $")
@@ -37,4 +38,22 @@ ggplot(data=HS_Dropout_by_County, aes(x=County, y=Rank.by.Percent, fill=County))
 county <- map_data("county")
 county <- filter(county, region == "washington")
 ggplot(county)+geom_polygon(aes(long,lat,group=group,col=subregion,fill=subregion))
+
+
+
+
+
+County_MedianListingPrice <- read.csv("data/County_MedianListingPrice_AllHomes.csv")
+County_MedianListingPrice <- filter(County_MedianListingPrice, State == "WA")
+County_MedianListingPrice_values <- County_MedianListingPrice[,c(1,118)]
+
+melted2 <- melt(County_MedianListingPrice_values)
+ggplot(melted2, aes(RegionName, value, fill=RegionName)) +
+  geom_bar(stat = "identity") +
+  ggtitle("Washington Median List Price by County in 2019") + 
+  xlab("County") +
+  ylab("Price $")
+# filter this based on their budget inputs
+
+
 
